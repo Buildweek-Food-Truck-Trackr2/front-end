55d1d78359808c15c5b2b61234e89f57890e4c45
masantea <mercyansong@gmail.com>
2020-05-20 19:59:17 -0400

    Create-React-App

---
+++ login/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+
+  render(){
+    return (
+      <div classname = "app">
+        This is the sleeptracker
+      </div>
+    )
+  }
+  
+}
+
+export default App;
