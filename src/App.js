c82ac38748fb28b75d0ded5f866682c871b46295
Laura reed <pilesoflaura@gmail.com>
2020-05-25 13:04:48 -0600

    If run, this code will have a routed nav and login page and home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Form from "./Components/Form";
+import { Route, Switch } from "react-router-dom";
+import Form from "./Components/Signup";
 import "./App.css";
+import Home from "./Components/modals/Home";
+import Login from "./Components/modals/Login";
+import Navigation from "./Components/modals/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Switch>
-        <Form />
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Form} />
       </Switch>
     </div>
   );
